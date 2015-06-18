2219a4bdabdfd7cc8a82d38a55a0b7eeb63708cd
GeekZP <1397819210@qq.com>
2015-06-18 11:31:43 +0800

    1.1
    1.1

--- ZPCusCount/ViewController.m
+++ ZPCusCount/ViewController.m
@@ -5,7 +5,7 @@
 //  Created by 郑鹏 on 15/6/18.
 //  Copyright (c) 2015年 pzheng. All rights reserved.
 //
-
+#import "CusSegment.h"
 #import "ViewController.h"
 
 @interface ViewController ()
@@ -16,7 +16,20 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    CusSegment *segView = [[CusSegment alloc] initWithFrame:CGRectMake(30, 100, 150, 35)];
+    
+//    segView.maxCount = @"30";
+//    segView.textCount = @"10";
+//    segView.lineColor = [UIColor redColor];
+//    segView.leftRightWidth = 100;
+//    segView.viewCornerRadius = 3;
+//    segView.isSupport = YES;
+    
+    [segView setCountChange:^(NSString *countStr) {
+        NSLog(@"%@",countStr);
+    }];
+    [self.view addSubview:segView];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -25,3 +38,14 @@
 }
 
 @end
+
+
+
+
+
+
+
+
+
+
+
